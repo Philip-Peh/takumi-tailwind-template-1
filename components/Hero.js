cd3ea454f0d03fe55eeaeb678be4b3eea7c636b3
Philip <philip@tk-international.com>
2023-11-09 16:43:40 +0800

    update hero path

--- components/Hero.js
+++ components/Hero.js
@@ -41,7 +41,7 @@ const Hero = ({
   <div className="flex w-3/6 sm:w-2/6 xl:w-1/6 h-3/6 items-center justify-center sm:justify-start text-center mx-auto">
     <motion.div className="w-full h-full" variants={scrollAnimation}>
       <Image
-        src="/assets/Logo.webp"
+        src="assets/Logo.webp"
         alt="VPN Illustration"
         quality={100}
         width={748}
